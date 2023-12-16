a60e1a3dbfdddb85b744583eea7bf10d022d8580
Afnan214 <afnan.ebrahim214@gmail.com>
2023-12-16 04:22:30 -0500

    CampusesStyle

--- src/components/views/CampusView/CampusView.js
+++ src/components/views/CampusView/CampusView.js
@@ -5,23 +5,27 @@ The Views component is responsible for rendering web page with data provided by 
 It constructs a React component to display a single campus and its students (if any).
 ================================================== */
 import { Link } from "react-router-dom";
-
+import './CampusView.css'
 // Take in props data to construct the component
 const CampusView = ({ campus }) => {
 
 
   // Render a single Campus view with list of its students
   return (
-    <div>
-      <h1>{campus.name}</h1>
-      <p>{campus.address}</p>
-      <p>{campus.description}</p>
+    <div className="CampusViewInfo">
+      <p className="CampusName">{campus.name}</p>
+      <img src={campus.imageUrl} className="CampusViewImage"></img>
+      <p>Address: </p>
+      <p className="CampusAddress">{campus.address}</p>
+      <p>Description: </p>
+      <p className="CampusDescription">{campus.description}</p>
+      <p className="StudentList">List of Students</p>
       {campus.students.map(student => {
         let name = student.firstname + " " + student.lastname;
         return (
           <div key={student.id}>
             <Link to={`/student/${student.id}`}>
-              <h2>{name}</h2>
+              <p>{name}</p>
             </Link>
           </div>
         );
